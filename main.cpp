9698de110c5829eb26c52558a16b68a3d9137279
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2024-12-01 17:01:46 -0600

    created main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string input;
+
+    while (true) {
+        cout << "Enter a string (or 'END' to quit): ";
+        cin >> input;
+
+        if (input == "END") {
+            break;
+        }
+
+        double number = extractNumeric(input);
+
+        if (number != -999999.99) {
+            cout << "The input is: " << fixed << setprecision(4) << number << endl;
+        } else {
+            cout << "The input is invalid." << endl;
+        }
+    }
+
+    return 0;
+}
